7fe8d390a5ac1a3cfd560106ee61d19dc85d8c34
Duulat Akmatbekov <duulat.akmatbekov@gmail.com>
2023-04-23 21:15:12 +0600

    Created backend part, installed starting libs, configured eslint, prettier and husky, added editorconfig file.

---
+++ backend/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import config from './config';
+
+const app = express();
+const port = 8000;
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+
+const run = async () => {
+  mongoose.set('strictQuery', false);
+  await mongoose.connect(config.db);
+
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+  process.on('exit', () => {
+    void mongoose.disconnect();
+  });
+};
+
+run().catch(console.error);
